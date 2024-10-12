7dfbd7b8c7edbdb38b2b7b00031b52cdb361ba8a
xiebaiyuan <xiebaiyuan@139.com>
2024-10-12 19:19:55 +0800

    添加更新zsh plugin的脚本

--- update_zsh_plugin.sh
+++ update_zsh_plugin.sh
@@ -1,6 +1,27 @@
-for plugin in ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/*; do
+#!/bin/zsh
+
+# 设置错误时退出
+set -e
+
+# 定义插件目录
+PLUGIN_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins"
+
+# 更新函数
+update_plugin() {
+  local plugin_name=$(basename "$1")
+  echo "正在更新 $plugin_name..."
+  if git -C "$1" pull --ff-only; then
+    echo "$plugin_name 更新成功"
+  else
+    echo "$plugin_name 更新失败"
+  fi
+}
+
+# 主循环
+for plugin in "$PLUGIN_DIR"/*; do
   if [ -d "$plugin/.git" ]; then
-    echo "正在更新 $(basename $plugin)..."
-    git -C "$plugin" pull
+    update_plugin "$plugin"
   fi
 done
+
+echo "所有插件更新完成"
